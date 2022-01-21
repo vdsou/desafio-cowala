c6ef98bce633871a4994404e40e307bda75a91ab
vdsou <vdsouza@outlook.com>
2022-01-21 15:10:36 -0300

    add header

---
+++ src/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <header>
+      <a href="/home">
+        <img src="logo.svg" alt="Logo" />
+      </a>
+    </header>
+  );
+}
+export default Header;
