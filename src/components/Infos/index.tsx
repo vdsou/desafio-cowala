560f34f6f96af17f59233fea953f6d271d979b3c
vdsou <vdsouza@outlook.com>
2022-01-22 16:08:54 -0300

    add info clear function

--- src/components/Infos/index.tsx
+++ src/components/Infos/index.tsx
@@ -1,38 +1,91 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 import "./styles.css";
 
+interface IData {
+  name: string;
+  profession: string;
+  fone: string;
+  ip: string;
+}
 function Infos() {
+  const initialData = {
+    name: String(""),
+    profession: String(""),
+    fone: String(""),
+    ip: String(""),
+  };
+  const [data, setData] = useState<Partial<IData>>();
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name: inputName, value } =
+      event.target as typeof event.target & {
+        name: { value: string };
+        value: { value: string };
+      };
+    setData({
+      ...data,
+      [inputName]: event.target.value,
+    });
+  };
+  const clear = () => {
+    setData(initialData);
+  };
   return (
     <main>
       <form>
         <label htmlFor="name">
           Nome
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={data?.name || ""}
+            onChange={handleChange}
+          />
         </label>
 
         <div className="tel-prof">
           <label htmlFor="profession">
             Profissão
-            <input type="text" id="profession" />
+            <input
+              type="text"
+              id="profession"
+              name="profession"
+              value={data?.profession || ""}
+              onChange={handleChange}
+            />
           </label>
 
           <label htmlFor="fone">
             Celular
-            <input type="text" id="fone" />
+            <input
+              type="tel"
+              id="fone"
+              name="fone"
+              value={data?.fone || ""}
+              onChange={handleChange}
+            />
           </label>
         </div>
 
         <div className="ip">
           <label htmlFor="ip-address">
             Meu IP
-            <input type="text" id="ip-address" />
+            <input
+              type="text"
+              id="ip-address"
+              name="ip"
+              value={data?.ip || ""}
+              onChange={handleChange}
+            />
           </label>
           <button type="button">ENCONTRAR IP</button>
         </div>
 
         <div className="buttons">
           <button type="submit">SALVAR</button>
-          <button type="button">LIMPAR</button>
+          <button type="button" onClick={clear}>
+            LIMPAR
+          </button>
         </div>
       </form>
     </main>
